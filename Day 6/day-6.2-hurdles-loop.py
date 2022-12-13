2882164f9ccf4399273effbc4deef252b93df247
Pavitra Mehta <pavitra.mehta@theparentinc.com>
2022-12-13 23:02:57 +0530

    Python 101 - Day 6 - Functions

---
+++ Day 6/day-6.2-hurdles-loop.py
@@ -0,0 +1,26 @@
+'''
+Go to https://reeborg.ca/reeborg.html
+Select Hurdle 1 challenge 
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    
+while(not at_goal()):
+   move_jump()
+   turn_left()
+
+'''
+    
+
+
---
+++ Day 6/day-6.4-maze-1.py
@@ -0,0 +1,25 @@
+'''
+Go to https://reeborg.ca/reeborg.html
+Select Maze 1 challenge 
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+    
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+'''
+    
+    
+
+
